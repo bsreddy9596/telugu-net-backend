5d0eea8656ed45bb792f6a0ee5869a37befc647c
bsreddy9596 <bsreddy9596@gmail.com>
2025-10-04 12:46:14 +0530

    Fixed walletRoutes path issue for Render

--- scripts/initMongo.js
+++ scripts/initMongo.js
@@ -1,33 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
-//models
-
-const User = require('../models/User');
-const Merchant = require('../models/Merchant');
-const Transaction = require('../models/Transaction');
-const Ad = require('../models/Ad');
-
+const User = require("../models/User");
+const Merchant = require("../models/Merchant");
+const Transaction = require("../models/Transaction");
+const Ad = require("../models/Ad");
 
 async function main() {
-    try {
-        await mongoose.connect(process.env.DB_URL)
-        console.log('mongoDB connected')
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("mongoDB connected");
 
-        await User.init();
-        await Merchant.init();
-        await Transaction.init();
-        await Ad.init();
+    await User.init();
+    await Merchant.init();
+    await Transaction.init();
+    await Ad.init();
 
-        console.log(' collections & indexes created successfully');
-        process.exit(0);
-
-    } catch (error) {
-        console.error('migration Error:', error);
-        process(1);
-    }
-
+    console.log(" collections & indexes created successfully");
+    process.exit(0);
+  } catch (error) {
+    console.error("migration Error:", error);
+    process(1);
+  }
 }
 
-main()
+main();
